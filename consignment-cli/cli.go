e68da773a8cf741793783c9eaf40d12e760daaa3
Ricardo Domingos <ricardohsd@gmail.com>
2018-04-11 22:46:36 +0200

    Refactor service and cli to use micro framework

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
 	pb "github.com/ricardohsd/microservices-example/consignment-service/proto/consignment"
 	"google.golang.org/grpc"
 )
@@ -28,12 +30,14 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
+	cmd.Init()
+
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
 	}
 	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
 
 	file := defaultFilename
 	if len(os.Args) > 1 {
