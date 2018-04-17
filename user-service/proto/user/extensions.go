0ca1ca6c77dae4def6df0be4a6c61c8709954d27
Ricardo Domingos <ricardohsd@gmail.com>
2018-04-18 00:02:23 +0200

    Fix user-service proto extension

--- user-service/proto/user/extensions.go
+++ user-service/proto/user/extensions.go
@@ -6,9 +6,6 @@ import (
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid, err := uuid.NewV4()
-	if err != nil {
-		return err
-	}
+	uuid := uuid.NewV4()
 	return scope.SetColumn("Id", uuid.String())
 }
