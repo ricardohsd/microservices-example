c9fa66f721824b2994ad4f0e3b06420099ef6cab
Ricardo Domingos <ricardohsd@gmail.com>
2018-04-17 23:17:47 +0200

    Add user service and cli

---
+++ user-service/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	micro "github.com/micro/go-micro"
+	pb "github.com/ricardohsd/microservices-example/user-service/proto/user"
+)
+
+func main() {
+	db, err := CreateConnection()
+	defer db.Close()
+
+	if err != nil {
+		log.Fatalf("Could not connect to DB: %v", err)
+	}
+
+	db.AutoMigrate(&pb.User{})
+
+	repo := &UserRepository{db}
+
+	tokenService := &TokenService{repo}
+
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.user"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
+
+	pb.RegisterUserServiceHandler(srv.Server(), &service{repo, tokenService})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
